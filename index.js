53d61c7d09bbe7b5fb45a138659b31ade879df8d
drewkiimon <andrew.r.pagan@gmail.com>
2020-04-27 17:00:55 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+	console.log("a user connected");
+
+	socket.on("chat message", (msg) => {
+		io.emit("chat message", msg);
+	});
+
+	socket.on("disconnect", () => {
+		let msg = "A user has left :(";
+
+		console.log("user disconnected");
+		io.emit("disconnect", msg);
+	});
+});
+
+http.listen(3000, () => {
+	console.log("listening on *:3000");
+});
